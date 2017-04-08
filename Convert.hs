5919082892b3709c09a011e1c2b1ecfe62aba160
Kubik <svehl.jakub@gmail.com>
2017-04-09 01:20:51 +0200

    add validations and examples in Main.hs

--- Convert.hs
+++ Convert.hs
@@ -7,52 +7,92 @@ module Convert
 import Data.Char
 import Data.List.Split
 
--- convertFromDec
-conFromDec :: Int -> Int -> [Int]
+
+-- system decimal == 10  | bin == 2...
+type System = Int
+-- bin 0101 == [0,1,0,1] | decimal 10 = [1, 0] ...
+type GeneralNumber = [Int]
+-- nothing special... just integer (but were sure, that its in decimal system)
+type DecNumber = Int
+
+-- convert from decimal
+-- take system and decimal number and return convert GeneralNumber 
+conFromDec :: System -> DecNumber -> GeneralNumber
 conFromDec _ 0 = []
 conFromDec system x =
     conFromDec system (x `div` system) ++ [x `mod` system]
 
 
 --convertToDecimal
-conToDec :: Int -> [Int] -> Int
+-- ill take system and general number and return basic integer (in decimal system)
+conToDec :: System -> GeneralNumber -> DecNumber
 conToDec _ [] = 0
 conToDec system (x:xs) =
     x * system^ length xs + conToDec system xs
 
-
-somethingToSomething :: Int -> Int -> [Int] -> [Int]
+-- take system1 from and system2 to
+-- and convert general number from system1 to system2
+somethingToSomething :: System -> System -> GeneralNumber -> GeneralNumber
 somethingToSomething s1 s2 =
   conFromDec s2 . conToDec s1
 
 
--- parsing and decoding function
-arrToStrNum :: [Int] -> String
+-- === parsing and decoding function ===
+-- convert GeneralNumber to String (A == 10 | B ==11 ... )
+arrToStrNum :: GeneralNumber -> String
 arrToStrNum =
   let mapFunc i = if i < 10 then chr (i + ord '0')
                             else (toUpper . chr) (i - 10 + ord 'a')
   in map mapFunc
 
-
-parseHigherSystems :: String -> [Int]
+-- parse NumberInString to GeneralNumber ("12A" => [1,2,10])
+-- max limit is 35... -> no abbrevation for another special chars
+parseHigherSystems :: String -> GeneralNumber
 parseHigherSystems =
-  let mapFunc i = if (ord i  - ord '0' ) < 10  then ord i - ord '0'
-                                               else 10 + ord i - ord 'A'
+  let mapFunc i = if (ord i - ord '0' ) < 10  then ord i - ord '0'
+                                              else 10 + ord i - ord 'A'
   in map (mapFunc . toUpper)
 
 
--- public api for user
-convertNum :: Int -> Int -> String -> String
-convertNum s1 s2 val = arrToStrNum $ somethingToSomething s1 s2 (parseHigherSystems val)
+-- === validations ===
+-- check if General Number is valid in Current System
+-- [0,1,0,1] is valid in system 2
+-- [0,1,0,9] isnt valid in system 2
+validMetrix :: System -> GeneralNumber -> Bool
+validMetrix system value =
+  not $ False `elem` (map (<system) value )
 
-convertNumByArrays :: Int -> Int -> [Int] -> [Int]
-convertNumByArrays = somethingToSomething
+
+-- public api for user
+-- convert number from System1 to System2
+-- input is for example "1010" and return is "A"
+-- max limit is 35 system...
+convertNum :: System -> System -> String -> Maybe String
+convertNum s1 s2 val = 
+  let arrNum = parseHigherSystems val
+  in 
+    if validMetrix s1 arrNum
+        then Just $ arrToStrNum $ somethingToSomething s1 s2 arrNum
+        else Nothing
+  
+
+-- take generalNumber in system1 and rerurn convert number to system 2
+convertNumByArrays :: System -> System -> GeneralNumber -> Maybe GeneralNumber
+convertNumByArrays s1 s2 val = 
+  if validMetrix s1 val 
+    then Just $ somethingToSomething s1 s2 val
+    else Nothing
+
 
 -- third variant is split digit by space -> 60 systems -> "54 10 30 45"
-convertNumBySpaces :: Int -> Int -> String -> String
+-- (System1 -> System2) => (2-16) take String like "1 0 1 0" and return for example 16 "A"
+convertNumBySpaces :: System -> System -> String -> Maybe String
 convertNumBySpaces s1 s2 val =
-  arrToStrNum $ somethingToSomething s1 s2 $ map (\i -> read i :: Int) (splitOn " " val)
-
+  let arrNum = map (\i -> read i :: Int) (splitOn " " val)
+  in 
+    if validMetrix s1 arrNum
+      then Just $ arrToStrNum $ somethingToSomething s1 s2 arrNum
+      else Nothing
 
 --validation
 -- TODO
--- Main.hs
+++ Main.hs
@@ -3,8 +3,34 @@ module Main where
 import Convert
 
 
--- test for me ( developer :D )
+import Data.Char
+
+
+  -- test for me ( developer :D )
+
+
 main :: IO ()
-main = -- do
-  putStrLn $ "vysledek čísla je: "
-      ++ show ( convertNum 8 16 "11057")
+main =  do
+  case convertNum 2 16 "1010A" of
+    Nothing -> putStrLn "Invalid inputs"
+    Just q  -> putStrLn q
+
+  case convertNum 2 16 "1010" of
+    Nothing -> putStrLn "Invalid inputs"
+    Just q  -> putStrLn q
+
+  case convertNumByArrays 2 16 [1,0,1,0,2] of
+    Nothing -> putStrLn "Invalid inputs"
+    Just q  -> putStrLn $ show q
+
+  case convertNumByArrays 2 16 [1,0,1,0] of
+    Nothing -> putStrLn "Invalid inputs"
+    Just q  -> putStrLn $ show q
+
+  case convertNumBySpaces 2 16 "1 0 1 0 9" of
+    Nothing -> putStrLn "Invalid inputs"
+    Just q  -> putStrLn $ show q
+
+  case convertNumBySpaces 2 16 "1 0 1 0" of
+    Nothing -> putStrLn "Invalid inputs"
+    Just q  -> putStrLn $ show q
