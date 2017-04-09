5c0d4b0b89d665c5ed62ffbf5c8fdf1604e7cdae
Jakub Švehla <svehl.jakub@gmail.com>
2017-04-09 20:06:57 +0200

    remove redundant code

--- Convert.hs
+++ Convert.hs
@@ -2,13 +2,14 @@ module Convert
 ( convertNumByArrays
 , convertNum
 , convertNumBySpaces
-, validBoolArr
 ) where
 
 import Data.Char
 import Data.List.Split
 
-
+-- ==================================
+-- === aliases for more type code ===
+-- ========================================
 -- system decimal == 10  | bin == 2...
 type System = Int
 -- bin 0101 == [0,1,0,1] | decimal 10 = [1, 0] ...
@@ -21,23 +22,15 @@ type SpecialCharsNum = String
 type SpaceSplitInt = String
 
 
-
-
-
--- helpers functions
--- check if array contain unvalid type (False...)
-validBoolArr :: [Bool] -> Bool
-validBoolArr = not . (elem False)
-
-
-
+-- ========================================
+-- ========= main logic functions =========
+-- ========================================
 -- convert from decimal
--- take system and decimal number and return convert GeneralNumber 
+-- take system and decimal number and return convert GeneralNumber
 conFromDec :: System -> DecNumber -> GeneralNumber
 conFromDec _ 0 = []
 conFromDec system x =
     conFromDec system (x `div` system) ++ [x `mod` system]
-
 
 --convertToDecimal
 -- ill take system and general number and return basic integer (in decimal system)
@@ -53,27 +46,16 @@ somethingToSomething s1 s2 =
   conFromDec s2 . conToDec s1
 
 
+
+-- =====================================
 -- === parsing and decoding function ===
--- convert GeneralNumber to String (A == 10 | B ==11 ... )
+-- =====================================
+-- convert GeneralNumber to String (A == 10 | B == 11 ... )
 arrToStrNum :: GeneralNumber -> String
 arrToStrNum =
   let mapFunc i = if i < 10 then chr (i + ord '0')
                             else (toUpper . chr) (i - 10 + ord 'a')
   in map mapFunc
-
-
-
--- hasBoolInArr boolArr = not $ False `elem` boolArr
--- valid max system
-validMaxSystem :: String -> Bool
-validMaxSystem x = 
-  let myF i = (i `elem` ['0'..'9']) || (toUpper i  `elem` ['A'..'Z'])
-  in validBoolArr $ map myF x 
-
-
-
-
-
 
 -- parse NumberInString to GeneralNumber ("12A" => [1,2,10])
 -- max limit is 35... -> no abbrevation for another special chars
@@ -84,54 +66,61 @@ parseHigherSystems =
   in map (mapFunc . toUpper)
 
 
--- === validations ===
+
+-- =====================================
+-- =========== validations =============
+-- =====================================
 -- check if General Number is valid in Current System
--- [0,1,0,1] is valid in system 2
--- [0,1,0,9] isnt valid in system 2
+-- [0,1,0,1] is valid in system 2 && [0,1,0,9] isnt valid in system 2
 validMetrix :: System -> GeneralNumber -> Bool
-validMetrix system value =
-  validBoolArr $ map (<system) value 
+validMetrix system = all (<system)
+
+-- hasBoolInArr boolArr = not $ False `elem` boolArr -- valid max system
+validMaxSystem :: String -> Bool
+validMaxSystem = all $ ( `elem` ( ['0'..'9'] ++ ['A'..'Z'] )) . toUpper
 
 
 
--- public api for user
+-- =================================
+-- ====== public api for user ======
+-- =================================
 -- convert number from System1 to System2
 -- input is for example "1010" and return is "A"
 -- max limit is 35 system...
 convertNum :: System -> System -> SpecialCharsNum -> Maybe SpecialCharsNum
-convertNum s1 s2 val = 
+convertNum _ 1 _ = Nothing -- cant use 1 system
+-- convertNum 1 _ _ = Nothing -- cant use 1 system
+convertNum s1 s2 val =
   let arrNum = parseHigherSystems val
-  in 
+  in
     if validMetrix s1 arrNum && validMaxSystem val
         then Just $ arrToStrNum $ somethingToSomething s1 s2 arrNum
         else Nothing
-  
+
 
 -- take generalNumber in system1 and rerurn convert number to system 2
 convertNumByArrays :: System -> System -> GeneralNumber -> Maybe GeneralNumber
-convertNumByArrays s1 s2 val = 
-  if validMetrix s1 val 
+convertNumByArrays s1 s2 val =
+  if validMetrix s1 val
     then Just $ somethingToSomething s1 s2 val
     else Nothing
 
 
--- third variant is split digit by space -> 60 systems -> "54 10 30 45"
+-- plit digit by space -> e.q. 60 systems -> "54 10 30 45"
 -- (System1 -> System2) => (2-16) take String like "1 0 1 0" and return for example 16 "A"
 -- different inputs and outputs... inputs string split by " " and outup is specialCharsNum...
 -- must fix..
 convertNumBySpaces :: System -> System -> String -> Maybe String
 convertNumBySpaces s1 s2 val =
-  let arrNum = map (\i -> read i :: Int) (splitOn " " val)
-  in 
-    if validMetrix s1 arrNum
+  let arrNum = map read $ splitOn " " val
+  in if validMetrix s1 arrNum
       then Just $ arrToStrNum $ somethingToSomething s1 s2 arrNum
       else Nothing
 
---validation
+
+
 -- TODO
--- last step for basic validations....
 -- create options for inputs and ouputs...
-    -- spaces | arrays | string (limit 35 = length (alphabet + 0..9))
 
 
 
@@ -141,4 +130,4 @@ convertNumBySpaces s1 s2 val =
 
 
 
---
+-- stay atom... remove spaces on end of file :D
--- Main.hs
+++ Main.hs
@@ -15,6 +15,10 @@ main =  do
     Nothing -> putStrLn "Invalid inputs"
     Just q  -> putStrLn q
 
+  --case convertNum 10 1 "1010" of
+  --  Nothing -> putStrLn "Invalid inputs"
+  --  Just q  -> putStrLn q
+
   case convertNum 2 16 "1010" of
     Nothing -> putStrLn "Invalid inputs"
     Just q  -> putStrLn q
@@ -25,16 +29,16 @@ main =  do
 
   case convertNumByArrays 2 16 [1,0,1,0,2] of
     Nothing -> putStrLn "Invalid inputs"
-    Just q  -> putStrLn $ show q
+    Just q  -> print q
 
   case convertNumByArrays 2 16 [1,0,1,0] of
     Nothing -> putStrLn "Invalid inputs"
-    Just q  -> putStrLn $ show q
+    Just q  -> print q
 
   case convertNumBySpaces 2 16 "1 0 1 0 9" of
     Nothing -> putStrLn "Invalid inputs"
-    Just q  -> putStrLn $ show q
+    Just q  -> print q
 
   case convertNumBySpaces 2 16 "1 0 1 0" of
     Nothing -> putStrLn "Invalid inputs"
-    Just q  -> putStrLn $ show q
+    Just q  -> print q
